9141d9a98189e1a3df534524f04e306ec5c123b2
Your Name <you@example.com>
2024-03-25 15:23:56 +0000

    Add logot option

--- static/theme-switcher.js
+++ static/theme-switcher.js
@@ -1,5 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", function () {
+    console.log("Added theme switcher")
     const localStorageTheme = localStorage.getItem("theme");
     const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
     
@@ -16,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     checkBox.addEventListener("click", () => {
-
+        console.log("Theme switcher triggered")
         let theme = document.querySelector("html").getAttribute("data-theme")
 
         if(theme === "light") {
